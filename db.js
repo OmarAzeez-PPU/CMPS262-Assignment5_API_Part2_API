b754171f413ab0efbf57b5c2fb356387184394df
Omar Azeez <Software.Omar.Azeez@outlook.com>
2024-09-18 13:30:28 -0400

    testing render config

--- db.js
+++ db.js
@@ -2,17 +2,17 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
 
-    // user: "postgres",
-    user: "oazeez",
+    user: "postgres",
+    // user: "oazeez",
 
-    // host: "localhost",
-    host: "dpg-crangalds78s73d73a40-a",
+    host: "localhost",
+    // host: "dpg-crangalds78s73d73a40-a",
 
-    // database: "inventory",
-    database: "inventory_lais",
+    database: "inventory",
+    // database: "inventory_lais",
 
-    // password: "PPU7$$7",
-    password: "v23InVINwOiyV5kFvpZ8kMxXynFbZAGE",
+    password: "PPU7$$7",
+    // password: "v23InVINwOiyV5kFvpZ8kMxXynFbZAGE",
 
     port: 5432,
 });
